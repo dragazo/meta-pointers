792c9e8155441540b2e4565811618131e16e6f48
dragazo <37459229+dragazo@users.noreply.github.com>
2018-12-30 12:40:47 -0600

    initial

---
+++ meta_ptr.h
@@ -0,0 +1,158 @@
+#ifndef DRAGAZO_META_PTR_H
+#define DRAGAZO_META_PTR_H
+
+#include <cstdint>
+#include <cstddef>
+#include <exception>
+#include <stdexcept>
+#include <type_traits>
+
+// represents a pointer to T with additional meta data stored in the least-significant bits.
+// specifically, n bits are available for meta data where n = log2(alignof(T)).
+// attempting to assign an unaligned pointer to this object results in an exception.
+template<typename T>
+class meta_ptr
+{
+private: // -- data -- //
+
+	std::uintptr_t raw; // the raw value containing the pointer and meta data
+
+private: // -- helpers -- //
+
+	// computes log2 at compile time - und if v is not a power of 2.
+	static constexpr std::size_t ctime_log2(std::size_t v) { return v <= 1 ? 0 : 1 + ctime_log2(v >> 1);}
+
+public: // -- ctor / dtor / asgn -- //
+
+	// constructs a meta ptr with zeroed meta data
+	constexpr meta_ptr(T *ptr = nullptr) { reset_clear(ptr); }
+
+	// assigns the pointer but doesn't modify the meta data
+	constexpr meta_ptr &operator=(T *ptr) { reset(ptr); return *this; }
+
+	// gets the stored pointer (with meta data stripped)
+	constexpr operator T*() const noexcept { return get(); }
+
+	// returns true iff the stored pointer is non-null
+	constexpr explicit operator bool() const noexcept { return get() != nullptr; }
+	// returns true iff the stored pointer is null
+	constexpr bool operator!() const noexcept { return get() == nullptr; }
+
+	// copies the pointer and meta data
+	meta_ptr(const meta_ptr&) = default;
+	// copies the pointer and meta data
+	meta_ptr &operator=(const meta_ptr&) = default;
+
+public: // -- pointer data -- //
+
+	// gets the stored pointer (with meta data stripped)
+	constexpr T *get() const noexcept
+	{
+		return (T*)(raw & ~(alignof(T) - 1));
+	}
+
+	// sets the stored pointer (preserves meta data).
+	// throws std::invalid_argument if ptr is not properly aligned for type T.
+	// if an exception is thrown, no change is made.
+	constexpr void reset(T *ptr = nullptr)
+	{
+		if ((std::uintptr_t)ptr & (alignof(T) - 1))
+			throw std::invalid_argument("ptr was not aligned");
+
+		raw = (std::uintptr_t)ptr | (raw & (alignof(T) - 1));
+	}
+
+	// sets the stored pointer and clears meta data to zero.
+	// throws std::invalid_argument if ptr is not properly aligned for type T.
+	// if an exception is thrown, no change is made.
+	constexpr void reset_clear(T *ptr = nullptr)
+	{
+		if ((std::uintptr_t)ptr & (alignof(T) - 1))
+			throw std::invalid_argument("ptr was not aligned");
+
+		raw = (std::uintptr_t)ptr;
+	}
+
+	constexpr T &operator*() const { return *get(); }
+	constexpr T *operator->() const noexcept { return get(); }
+	constexpr T &operator[](std::ptrdiff_t index) const { get()[index]; }
+
+public: // -- meta data -- //
+
+	// the number of available meta data bits
+	static constexpr std::size_t meta_bits = ctime_log2(alignof(T));
+
+	// reads the meta data.
+	// the low bits of the result contain the meta data (up to meta_bits).
+	// the high bits are zero.
+	constexpr std::size_t read() const noexcept
+	{
+		return raw & (alignof(T) - 1);
+	}
+	// writes the meta data (preserves the pointer value).
+	// the low bits are written to the meta data (up to meta_bits).
+	// the high bits are discarded.
+	constexpr void write(std::size_t v) noexcept
+	{
+		raw = raw & ~(alignof(T) - 1) | v & (alignof(T) - 1);
+	}
+
+	// sets (all) the meta data bits to 1
+	constexpr void set() noexcept
+	{
+		raw |= alignof(T) - 1;
+	}
+	// clears (all) the meta data bits to 0.
+	constexpr void clear() noexcept
+	{
+		raw &= ~(alignof(T) - 1);
+	}
+	// flips (all) the meta data bits.
+	constexpr void flip() noexcept
+	{
+		raw ^= alignof(T) - 1;
+	}
+
+public: // -- fine-tuned meta data -- //
+
+	// reads the meta data bit at positon pos (0 order)
+	template<std::size_t pos, std::enable_if_t<pos < meta_bits, int> = 0>
+	constexpr bool read() const noexcept
+	{
+		return raw & ((std::uintptr_t)1 << pos);
+	}
+	// writes the meta data bit at position pos (0 order)
+	template<std::size_t pos, std::enable_if_t<pos < meta_bits, int> = 0>
+	constexpr void write(bool val) noexcept
+	{
+		val ? set<pos>() : clear<pos>();
+	}
+
+	// sets the meta data bit at position pos (0 order)
+	template<std::size_t pos, std::enable_if_t<pos < meta_bits, int> = 0>
+	constexpr void set() noexcept
+	{
+		raw |= (std::uintptr_t)1 << pos;
+	}
+	// clears the meta data bit at position pos (0 order)
+	template<std::size_t pos, std::enable_if_t<pos < meta_bits, int> = 0>
+	constexpr void clear() noexcept
+	{
+		raw &= ~((std::uintptr_t)1 << pos);
+	}
+	// flips the meta data bit at position pos (0 order)
+	template<std::size_t pos, std::enable_if_t<pos < meta_bits, int> = 0>
+	constexpr void flip() noexcept
+	{
+		raw ^= (std::uintptr_t)1 << pos;
+	}
+
+public: // -- comparison -- //
+
+	// returns true iff the meta ptrs point to the same object AND have identical meta data
+	constexpr friend bool operator==(meta_ptr a, meta_ptr b) { return a.raw == b.raw; }
+	// returns true iff the meta ptrs point to different objects OR have different meta data
+	constexpr friend bool operator!=(meta_ptr a, meta_ptr b) { return a.raw != b.raw; }
+};
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include "meta_ptr.h"
+
+template<typename T>
+void align_check() { static_assert((1 << meta_ptr<T>::meta_bits) == alignof(T), "alignment error"); }
+
+int main()
+{
+	align_check<char>();
+	align_check<unsigned short>();
+	align_check<int>();
+	align_check<unsigned long>();
+	align_check<long long>();
+	align_check<float>();
+	align_check<double>();
+	align_check<long double>();
+	align_check<void*>();
+	align_check<int*const*>();
+	align_check<std::string>();
+	align_check<std::vector<int>>();
+	align_check<std::vector<std::string>>();
+
+	int _a, _b, _c, _d;
+	int *a = &_a, *b = &_b, *c = &_c, *d = &_d;
+
+	meta_ptr<int> ma, mb;
+	meta_ptr<int> mc(c);
+	meta_ptr<int> md(mc);
+
+	assert(ma.get() == nullptr && mb.get() == nullptr);
+	assert(!(bool)ma && !ma);
+	assert(!(bool)mb && !mb);
+	assert(ma.read() == 0 && mb.read() == 0);
+	assert(ma == mb && !(ma != mb));
+
+	assert(mc.get() == c && md.get() == c);
+	assert((bool)mc && !!mc);
+	assert((bool)md && !!md);
+	assert(mc.read() == 0 && md.read() == 0);
+	assert(mc == md && !(mc != md));
+
+	mc.write(2);
+	md.write(3);
+	assert(mc.read() == 2 && md.read() == 3);
+	assert(mc.get() == c && md.get() == c);
+	assert(mc != md && !(mc == md));
+
+	mc = d;
+	md.reset(d);
+	assert(mc.read() == 2 && md.read() == 3);
+	assert(mc.get() == d && md.get() == d);
+	assert(mc != md && !(mc == md));
+
+	ma = mc;
+	mb = md;
+	assert(ma.get() == mb.get());
+	assert(ma.read() == mc.read());
+	assert(mb.read() == md.read());
+	assert(ma == mc);
+	assert(mb == md);
+	assert(ma != mb);
+
+	ma.clear();
+	assert(ma.read() == 0);
+	assert(ma.get() == d);
+
+	assert(mb.read() != 0);
+	assert(mb.get() != a);
+	mb.reset_clear(a);
+	assert(mb.read() == 0);
+	assert(mb.get() == a);
+
+	assert(ma.read() == mb.read());
+	assert(ma.get() != mb.get());
+	assert(ma != mb);
+
+	ma.set<1>();
+	assert(!ma.read<0>());
+	assert(ma.read<1>());
+	assert(ma.read() == 2);
+	ma.set<0>();
+	assert(ma.read<0>());
+	assert(ma.read<1>());
+	assert(ma.read() == 3);
+	ma.clear<1>();
+	assert(ma.read<0>());
+	assert(!ma.read<1>());
+	assert(ma.read() == 1);
+	ma.clear<0>();
+	assert(!ma.read<0>());
+	assert(!ma.read<1>());
+	assert(ma.read() == 0);
+
+	ma.write(0);
+	mb.write(1);
+	mc.write(2);
+	md.write(3);
+	assert(ma.read() == 0 && mb.read() == 1 && mc.read() == 2 && md.read() == 3);
+
+	mb.reset(c);
+	assert(mb.get() == c && mb.read() == 1);
+	assert(mb != mc);
+
+	mb.write(0x125fe);
+	assert(mb.get() == c && mb.read() == 2);
+
+	mb.write<0>(true);
+	assert(mb.get() == c && mb.read() == 3);
+
+	mb.write<1>(false);
+	assert(mb.get() == c && mb.read() == 1);
+
+	mb.write<1>(true);
+	assert(mb.get() == c && mb.read() == 3);
+
+	mb.write<0>(false);
+	assert(mb.get() == c && mb.read() == 2);
+
+	mb.flip<0>();
+	assert(mb.get() == c && mb.read() == 3);
+
+	mb.flip<0>();
+	assert(mb.get() == c && mb.read() == 2);
+
+	mb.set();
+	assert(mb.get() == c && mb.read() == 3);
+
+	mb.flip();
+	assert(mb.get() == c && mb.read() == 0);
+
+	mb.set<1>();
+	mb.flip();
+	assert(mb.get() == c && mb.read() == 1);
+
+	mb.flip();
+	assert(mb.get() == c && mb.read() == 2);
+
+	std::cerr << "\n\ntests completed\n";
+
+	std::cin.get();
+	return 0;
+}
